e835a4cedc52a4c1abd70ba01baae9c8ef7c45d0
Bailey Morris <bailey.morris71@gmail.com>
2019-09-04 09:26:35 -0500

    final

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.kanye.rest";
+
+            HttpClient client = new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+
+            string quote = JObject.Parse(response).GetValue("quote").ToString();
+            Console.WriteLine(quote);
+        }
+       // static void Main()
+       // {
+            //string url = "https://api.chucknorris.io/jokes/random";
+
+           // HttpClient client = new HttpClient();
+           // string response = client.GetStringAsync(url).Result;
+
+           // string joke2 = JObject.Parse(response).GetValue("value").ToString();
+       // }   
+    }
+}
